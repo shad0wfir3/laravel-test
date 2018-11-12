f139ff7db5006d351d14693b77784170e92684eb
shad0wfir3 <eamon@symantic.co.za>
2018-11-12 20:44:35 +0200

    migrations, models and controllers created

---
+++ app/Http/Controllers/ArticleRevisionController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Article_Revision;
+use Illuminate\Http\Request;
+
+class ArticleRevisionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Article_Revision  $article_Revision
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Article_Revision $article_Revision)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Article_Revision  $article_Revision
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Article_Revision $article_Revision)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Article_Revision  $article_Revision
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Article_Revision $article_Revision)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Article_Revision  $article_Revision
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Article_Revision $article_Revision)
+    {
+        //
+    }
+}
